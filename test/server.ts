e78c59a001d3436687bc0a026785e10694eb7627
ishaf <shafiulislam20@gmail.com>
2023-01-28 02:31:52 +0600

    fix(table): table head

--- test/server.ts
+++ test/server.ts
@@ -4,30 +4,32 @@ import EditorJsToHtml from "../dist/cjs";
 const app = express()
 const ejs = new EditorJsToHtml();
 const server = createServer(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6969;
 app.get('/', (req, res) => res.sendFile('index.html', {root: app.get('views')}))
 app.get('/perse', async (req, res) => {
 
     res.send(ejs.parse({
-        "time": 1674775249942,
+        "time": 1674849838500,
         "blocks": [
             {
-                "id": "5dHkK2vsft",
-                "type": "code",
+                "id": "R8fWhGtl2s",
+                "type": "table",
                 "data": {
-                    "code": ".ce-code__textarea {\n    min-height: 200px;\n    font-family: Menlo, Monaco, Consolas, Courier New, monospace;\n    color: #41314e;\n    line-height: 1.6em;\n    font-size: 12px;\n    background: #f8f7fa;\n    border: 1px solid #f1f1f4;\n    box-shadow: none;\n    white-space: pre;\n    word-wrap: normal;\n    overflow-x: auto;\n    resize: vertical;\n}"
+                    "withHeadings": true,
+                    "content": [
+                        [
+                            "dvbdc",
+                            "cvbc",
+                            "cvb"
+                        ],
+                        [
+                            "cvb",
+                            "cvb",
+                            "cvb"
+                        ]
+                    ]
                 }
-            },
-            {
-                "type": "image",
-                "data": {
-                    "url": "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg",
-                    "caption": "Roadster // tesla.com",
-                    "withBorder": false,
-                    "withBackground": false,
-                    "stretched": true
-                }
-            },
+            }
         ],
         "version": "2.26.4"
     }))
